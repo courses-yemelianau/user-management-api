ba074c1bb3a39c3898a17c294932e6217e541439
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-03 01:38:19 +0300

    🔧 chore(database): refactor sequelize connection string to use URI format, remove commented code and unused disconnect function

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,25 +4,17 @@ import UserModel from '@models/users.model';
 import { logger } from '@utils/logger';
 import { Env } from '@constants';
 
-const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+const sequelize = new Sequelize.Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?ssl=true`, {
     dialect: 'postgres',
-    host: DB_HOST,
     port: +DB_PORT,
-    // timezone: '+09:00',
-    // define: {
-    //   charset: 'utf8mb4',
-    //   collate: 'utf8mb4_general_ci',
-    //   underscored: true,
-    //   freezeTableName: true,
-    // },
-    // pool: {
-    //   min: 0,
-    //   max: 5,
-    // },
+    pool: {
+        min: 0,
+        max: 5
+    },
     logQueryParameters: NODE_ENV === Env.Development,
-    // logging: (query, time) => {
-    //   logger.info(time + 'ms' + ' ' + query);
-    // },
+    logging: (query, time) => {
+        logger.info(`${time}ms ${query}`);
+    },
     benchmark: true
 });
 
@@ -34,10 +26,8 @@ sequelize.authenticate()
         logger.error(`Unable to connect to the database: ${error}.`);
     });
 
-export const disconnect = (callback) => sequelize.close().then(callback);
-
 export const DB = {
     Users: UserModel(sequelize),
-    sequelize, // connection instance (RAW queries)
-    Sequelize // library
+    sequelize,
+    Sequelize
 };
